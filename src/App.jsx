992a2e955783aa848db05ebf7bcde7add5b67047
Arun Singh <arunsingh28aug.as@gmail.com>
2021-11-18 15:27:30 +0530

    color

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Color from "./config/color";
 
 const App = () => {
   const pick = React.useRef();
+  const [color, setColor] = React.useState();
   return (
     <div>
       <input type="color" ref={pick} />
       <button
         onClick={() => {
-          console.log(pick.current.value);
-          Color(pick.current.value);
+          const c = Color(pick.current.value);
+          setColor(c);
         }}
       >
         Get
       </button>
+      {color ? color : "please select color"}
+
+      <input type="color" value={color} />
     </div>
   );
 };
--- src/config/color.js
+++ src/config/color.js
@@ -1,5 +1,16 @@
 const Color = (color) => {
-    return console.log("Color from Color function", color);
+    const nutral_color = ["#3b46e8", "#ffffff", "#000000", "#6c813a"];
+
+    nutral_color.map((item) => {
+        if (item === color) {
+            console.log(item);
+            // return diffrent color
+            return "#fff";
+        }
+    });
+
+    // return suggested color
+    // return color;
 };
 
 export default Color;
